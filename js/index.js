8d3fb1e8864c7d9e82a2531ee90bd047ea805988
iMac <imac@iMacs-iMac.local>
2021-06-04 16:21:23 +0400

    responsive

--- js/index.js
+++ js/index.js
@@ -2,11 +2,17 @@ sliderImg = document.querySelectorAll('.slidePagination img');
 mainSlide = document.querySelectorAll('.mainSlide .slide');
 activeIndex = 1;
 
+var interval = setInterval(changeSlide, 3000);
 
 for (let i = 0; i < sliderImg.length; i++) {
-    sliderImg[i].addEventListener("click", function(e) {
-        e.preventDefault();
-        activeIndex = i;
+    sliderImg[i].addEventListener("click", function () {
+        myStopFunction();
+
+        activeIndex = i + 1;
+        if (activeIndex == 5) {
+            activeIndex = 0;
+        }
+        interval = setInterval(changeSlide, 3000);
         activeSliderImg = document.querySelector('.slidePagination .activeImg').classList.remove('activeImg');
         this.classList.add('activeImg');
         document.querySelector('.mainSlide .activeSlide').classList.remove('activeSlide');
@@ -15,15 +21,38 @@ for (let i = 0; i < sliderImg.length; i++) {
 }
 
 
-setInterval(function() {
-
+function changeSlide() {
     activeSliderImg = document.querySelector('.slidePagination .activeImg').classList.remove('activeImg');
     sliderImg[activeIndex].classList.add('activeImg');
     document.querySelector('.mainSlide .activeSlide').classList.remove('activeSlide');
     mainSlide[activeIndex].classList.add('activeSlide');
     activeIndex += 1;
-
     if (activeIndex == 5) {
         activeIndex = 0;
     }
-}, 3000);
+}
+
+function myStopFunction() {
+    clearInterval(interval);
+}
+
+menu = $(".menuButton");
+navbarCollapse = $('.navbar-collapse');
+menu.click(function () {
+    navbarCollapse.toggleClass('opened');
+    $('.menuIcon').toggleClass('open');
+})
+
+
+
+function myFunction(x) {
+    if (x.matches) {
+        $('.nav-item').click(function () {
+            $(this).find('.navDropDown').slideToggle();
+        })
+    }
+}
+
+var x = window.matchMedia("(max-width: 1200px)")
+myFunction(x)
+x.addListener(myFunction)
